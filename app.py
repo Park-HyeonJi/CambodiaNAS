1a515031fa3e9bc2d13785f3308b55f56347e211
Park-HyeonJi <phjgina0314@gmail.com>
2024-06-24 15:54:20 +0900

    flask 틀 잡아두기, 로그인 page로 회귀 이슈 해결 필요

--- app.py
+++ app.py
@@ -1,10 +1,85 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'base'
+
+# 사용자 데이터베이스 시뮬레이션
+users = {'ddd': {'password': 'password'}}
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def base():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return render_template('base.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    if username in users and users[username]['password'] == password:
+        user = User(username)
+        login_user(user)
+        return redirect(url_for('home'))
+    return redirect(url_for('base'))
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+@app.route('/m-group')
+@login_required
+def m_group():
+    return render_template('m_group.html')
+
+@app.route('/m-person')
+@login_required
+def m_person():
+    return render_template('m_person.html')
+
+@app.route('/24h')
+@login_required
+def tfh():
+    return render_template('24h.html')
+
+@app.route('/24h-stas')
+@login_required
+def tfh_stas():
+    return render_template('24h_stas.html')
+
+@app.route('/24h-excel')
+@login_required
+def tfh_excel():
+    return render_template('24h_excel.html')
+
+@app.route('/db-food')
+@login_required
+def db_food():
+    return render_template('db_food.html')
+
+@app.route('/db-nutri')
+@login_required
+def db_nutri():
+    return render_template('db_nutri.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('base'))
 
 if __name__ == '__main__':
     app.run(debug=True)
