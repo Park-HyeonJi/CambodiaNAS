d4842971a68600244e145869d745c72757a29bcd
kayeon2 <tjsrkdus@gmail.com>
2024-08-20 14:36:10 +0900

    fix: 코드 위치 재배치

--- static/24h.js
+++ static/24h.js
@@ -7,6 +7,51 @@ document.addEventListener('DOMContentLoaded', () => {
     setActive(defaultCategory);
 });
 
+// 1행: 그룹 및 유저
+function loadGroupsForUser() {
+    fetch('/get_groups')
+        .then(response => response.json())
+        .then(groups => {
+            const groupSelect = document.getElementById('userGroup');
+            groupSelect.innerHTML = '';
+
+            groups.forEach(group => {
+                const option = document.createElement('option');
+                option.value = group.name;
+                option.textContent = group.name;
+                groupSelect.appendChild(option);
+            });
+
+            loadUsers();
+        })
+        .catch(error => console.error('Error loading groups for users:', error));
+}
+
+function loadUsers() {
+    const selectedGroup = document.getElementById('userGroup').value;
+    
+    fetch(`/get_users?group=${selectedGroup}`)
+        .then(response => response.json())
+        .then(users => {
+            const userSelect = document.getElementById('userID');
+            userSelect.innerHTML = '';
+
+            users.forEach(user => {
+                const option = document.createElement('option');
+                option.value = user.id;
+                option.textContent = user.name;
+                userSelect.appendChild(option);
+            });
+
+            selectedUserId = null;
+        })
+        .catch(error => console.error('Error loading users:', error));
+}
+
+function loadData() {
+    loadFoodList();   // 음식 리스트를 불러옵니다.
+}
+
 // time-categroeis 버튼 클릭 시 호출
 function setActive(button) {
     document.querySelectorAll('.time-categories button').forEach(btn => btn.classList.remove('active'));
@@ -14,6 +59,189 @@ function setActive(button) {
 
     // 음식 리스트 초기화
     loadFoodList();
+}
+
+// 첫 번째 컨테이너
+function loadFoodList() {
+    var userGroup = document.getElementById('userGroup').value;
+    var userID = document.getElementById('userID').value;
+    var viewDate = document.getElementById('date').value;
+    var activeCategory = document.querySelector('.time-categories button.active').dataset.category;
+
+    fetch(`/get_food_list?userGroup=${userGroup}&userID=${userID}&viewDate=${viewDate}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log('Fetched data:', data); // 데이터 확인을 위해 추가
+        document.querySelectorAll('.food-items tbody').forEach(tbody => tbody.innerHTML = ""); // 기존 데이터를 초기화
+
+        var tableBody = document.querySelector(`.food-items tbody[data-category='${activeCategory}']`);
+        var uniqueFoods = [];
+
+        data.forEach(food => {
+            if (food['TIME'] === activeCategory && !uniqueFoods.some(item => item['FOODNAME'] === food['FOODNAME'])) {
+                uniqueFoods.push(food);
+                var tr = document.createElement("tr");
+                tr.innerHTML = `<td>${food['FOODID']}</td><td>${food['FOODNAME']}</td>`;
+
+                tr.onclick = function() {
+                    tableBody.querySelectorAll('tr').forEach(r => r.style.backgroundColor = '');
+                    tr.style.backgroundColor = 'lightgray';
+        
+                    // 행을 클릭할 때 재료 로드
+                    loadIngredients(food);
+                };
+                tableBody.appendChild(tr);
+            }
+        });
+        loadAllFoodList();
+    });
+}
+
+// 두 번째 컨테이너
+function toggleInputFields() {
+    var codeInput = document.getElementById("codeInput");
+    var nameInput = document.getElementById("nameInput");
+    var searchType = document.querySelector('input[name="searchType"]:checked').value;
+
+    if (searchType === "code") {
+        codeInput.disabled = false;
+        nameInput.disabled = true;
+    } else {
+        codeInput.disabled = true;
+        nameInput.disabled = false;
+    }
+}
+
+// Pagination
+let currentPage = 1;
+const limit = 10; // 페이지당 표시할 아이템 수
+let totalPages = 1;
+let currentSearchResults = []; // 현재 검색 결과를 저장하는 배열
+
+function searchFood() {
+    var searchType = document.querySelector('input[name="searchType"]:checked').value;
+    var searchValue = (searchType === "code") ? document.getElementById("codeInput").value : document.getElementById("nameInput").value;
+
+    fetch('/search_food', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ searchType: searchType, searchValue: searchValue })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data)
+        // 중복 제거
+        const uniqueResults = [];
+        const seenCodes = new Set();
+
+        data.forEach(row => {
+            if (!seenCodes.has(row['FOODID'])) {
+                seenCodes.add(row['FOODID']);
+                uniqueResults.push(row);
+            }
+        });
+
+        currentSearchResults = uniqueResults;
+        totalPages = Math.ceil(currentSearchResults.length / limit);
+        currentPage = 1; // 검색 후 첫 페이지로 이동
+        renderSearchResults(); // 페이지를 초기화하며 결과를 렌더링
+    });
+}
+
+function renderSearchResults() {
+    const tableBody = document.querySelector("#foodTable tbody");
+    tableBody.innerHTML = ""; // 기존 데이터를 초기화
+
+    const start = (currentPage - 1) * limit;
+    const end = start + limit;
+    const resultsToShow = currentSearchResults.slice(start, end);
+
+    resultsToShow.forEach(row => {
+        var tr = document.createElement("tr");
+        tr.innerHTML = `<td>${row['FOODID']}</td><td>${row['FOODNAME']}</td>`;
+        tr.onclick = function() {
+            tableBody.querySelectorAll('tr').forEach(r => r.style.backgroundColor = '');
+            tr.style.backgroundColor = 'lightgray';
+            tr.dataset.selected = true;
+        };
+        tableBody.appendChild(tr);
+    });
+
+    document.getElementById('prevPageFoodBtn').disabled = currentPage === 1;
+    document.getElementById('nextPageFoodBtn').disabled = currentPage === totalPages;
+}
+
+document.getElementById('prevPageFoodBtn').addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage--;
+        renderSearchResults();
+    }
+});
+
+document.getElementById('nextPageFoodBtn').addEventListener('click', () => {
+    if (currentPage < totalPages) {
+        currentPage++;
+        renderSearchResults();
+    }
+});
+
+function addToFoodList() {
+    var selectedRow = document.querySelector("#foodTable tbody tr[data-selected='true']");
+    if (!selectedRow) return alert("Please select a food item to add");
+
+    var foodCode = selectedRow.cells[0].textContent;
+    var foodName = selectedRow.cells[1].textContent;
+    var timeCategory = document.querySelector('.time-categories button.active').dataset.category;
+    var userGroup = document.getElementById('userGroup').value;
+    var userID = document.getElementById('userID').value;
+    var viewDate = document.getElementById('date').value;
+
+    fetch('/add_food', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            foodCode: foodCode,
+            foodName: foodName,
+            timeCategory: timeCategory,
+            userGroup: userGroup,
+            userID: userID,
+            viewDate: viewDate
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            loadFoodList();
+            // loadAllFoodList();
+        } else {
+            alert("Error adding food: " + data.message);
+        }
+    });
+}
+
+// 세 번째 컨테이너
+function loadIngredients(food) {
+    // ingredientTableBody를 초기화합니다.
+    var ingredientTableBody = document.getElementById('ingredientTableBody');
+    ingredientTableBody.innerHTML = "";
+
+    // 음식의 재료 정보를 가져옵니다.
+    fetch(`/get_food_ingredients?foodCode=${food['FOODID']}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(ingredient => {
+            var tr = document.createElement("tr");
+            tr.innerHTML = `<td>${ingredient['INGID']}</td>
+            <td>${ingredient['INGNAME_EN']}</td>
+            <td>${ingredient['1 person (g)']}</td>`;
+            ingredientTableBody.appendChild(tr);
+        });
+    });
 }
 
 // 3행: 영양 성분 테이블
@@ -205,238 +433,6 @@ function appendTotalRow(totalNutrients) {
     } else {
         console.error('Element with ID "nutrition-tbody" not found');
     }
-}
-
-// 1행: 그룹 및 유저
-function loadGroupsForUser() {
-    fetch('/get_groups')
-        .then(response => response.json())
-        .then(groups => {
-            const groupSelect = document.getElementById('userGroup');
-            groupSelect.innerHTML = '';
-
-            groups.forEach(group => {
-                const option = document.createElement('option');
-                option.value = group.name;
-                option.textContent = group.name;
-                groupSelect.appendChild(option);
-            });
-
-            loadUsers();
-        })
-        .catch(error => console.error('Error loading groups for users:', error));
-}
-
-function loadUsers() {
-    const selectedGroup = document.getElementById('userGroup').value;
-    
-    fetch(`/get_users?group=${selectedGroup}`)
-        .then(response => response.json())
-        .then(users => {
-            const userSelect = document.getElementById('userID');
-            userSelect.innerHTML = '';
-
-            users.forEach(user => {
-                const option = document.createElement('option');
-                option.value = user.id;
-                option.textContent = user.name;
-                userSelect.appendChild(option);
-            });
-
-            selectedUserId = null;
-        })
-        .catch(error => console.error('Error loading users:', error));
-}
-
-// 두 번째 컨테이너
-function toggleInputFields() {
-    var codeInput = document.getElementById("codeInput");
-    var nameInput = document.getElementById("nameInput");
-    var searchType = document.querySelector('input[name="searchType"]:checked').value;
-
-    if (searchType === "code") {
-        codeInput.disabled = false;
-        nameInput.disabled = true;
-    } else {
-        codeInput.disabled = true;
-        nameInput.disabled = false;
-    }
-}
-
-// Pagination
-let currentPage = 1;
-const limit = 10; // 페이지당 표시할 아이템 수
-let totalPages = 1;
-let currentSearchResults = []; // 현재 검색 결과를 저장하는 배열
-
-function searchFood() {
-    var searchType = document.querySelector('input[name="searchType"]:checked').value;
-    var searchValue = (searchType === "code") ? document.getElementById("codeInput").value : document.getElementById("nameInput").value;
-
-    fetch('/search_food', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ searchType: searchType, searchValue: searchValue })
-    })
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data)
-        // 중복 제거
-        const uniqueResults = [];
-        const seenCodes = new Set();
-
-        data.forEach(row => {
-            if (!seenCodes.has(row['FOODID'])) {
-                seenCodes.add(row['FOODID']);
-                uniqueResults.push(row);
-            }
-        });
-
-        currentSearchResults = uniqueResults;
-        totalPages = Math.ceil(currentSearchResults.length / limit);
-        currentPage = 1; // 검색 후 첫 페이지로 이동
-        renderSearchResults(); // 페이지를 초기화하며 결과를 렌더링
-    });
-}
-
-function renderSearchResults() {
-    const tableBody = document.querySelector("#foodTable tbody");
-    tableBody.innerHTML = ""; // 기존 데이터를 초기화
-
-    const start = (currentPage - 1) * limit;
-    const end = start + limit;
-    const resultsToShow = currentSearchResults.slice(start, end);
-
-    resultsToShow.forEach(row => {
-        var tr = document.createElement("tr");
-        tr.innerHTML = `<td>${row['FOODID']}</td><td>${row['FOODNAME']}</td>`;
-        tr.onclick = function() {
-            tableBody.querySelectorAll('tr').forEach(r => r.style.backgroundColor = '');
-            tr.style.backgroundColor = 'lightgray';
-            tr.dataset.selected = true;
-        };
-        tableBody.appendChild(tr);
-    });
-
-    document.getElementById('prevPageFoodBtn').disabled = currentPage === 1;
-    document.getElementById('nextPageFoodBtn').disabled = currentPage === totalPages;
-}
-
-document.getElementById('prevPageFoodBtn').addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage--;
-        renderSearchResults();
-    }
-});
-
-document.getElementById('nextPageFoodBtn').addEventListener('click', () => {
-    if (currentPage < totalPages) {
-        currentPage++;
-        renderSearchResults();
-    }
-});
-
-function addToFoodList() {
-    var selectedRow = document.querySelector("#foodTable tbody tr[data-selected='true']");
-    if (!selectedRow) return alert("Please select a food item to add");
-
-    var foodCode = selectedRow.cells[0].textContent;
-    var foodName = selectedRow.cells[1].textContent;
-    var timeCategory = document.querySelector('.time-categories button.active').dataset.category;
-    var userGroup = document.getElementById('userGroup').value;
-    var userID = document.getElementById('userID').value;
-    var viewDate = document.getElementById('date').value;
-
-    fetch('/add_food', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            foodCode: foodCode,
-            foodName: foodName,
-            timeCategory: timeCategory,
-            userGroup: userGroup,
-            userID: userID,
-            viewDate: viewDate
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 'success') {
-            loadFoodList();
-            // loadAllFoodList();
-        } else {
-            alert("Error adding food: " + data.message);
-        }
-    });
-}
-
-// 첫 번째 컨테이너
-function loadFoodList() {
-    var userGroup = document.getElementById('userGroup').value;
-    var userID = document.getElementById('userID').value;
-    var viewDate = document.getElementById('date').value;
-    var activeCategory = document.querySelector('.time-categories button.active').dataset.category;
-
-    fetch(`/get_food_list?userGroup=${userGroup}&userID=${userID}&viewDate=${viewDate}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log('Fetched data:', data); // 데이터 확인을 위해 추가
-        document.querySelectorAll('.food-items tbody').forEach(tbody => tbody.innerHTML = ""); // 기존 데이터를 초기화
-
-        var tableBody = document.querySelector(`.food-items tbody[data-category='${activeCategory}']`);
-        var uniqueFoods = [];
-
-        data.forEach(food => {
-            if (food['TIME'] === activeCategory && !uniqueFoods.some(item => item['FOODNAME'] === food['FOODNAME'])) {
-                uniqueFoods.push(food);
-                var tr = document.createElement("tr");
-                tr.innerHTML = `<td>${food['FOODID']}</td><td>${food['FOODNAME']}</td>`;
-
-                tr.onclick = function() {
-                    tableBody.querySelectorAll('tr').forEach(r => r.style.backgroundColor = '');
-                    tr.style.backgroundColor = 'lightgray';
-        
-                    // 행을 클릭할 때 재료 로드
-                    loadIngredients(food);
-                };
-                tableBody.appendChild(tr);
-            }
-        });
-        loadAllFoodList();
-    });
-}
-
-// 세 번째 컨테이너
-function loadIngredients(food) {
-    // ingredientTableBody를 초기화합니다.
-    var ingredientTableBody = document.getElementById('ingredientTableBody');
-    ingredientTableBody.innerHTML = "";
-
-    // 음식의 재료 정보를 가져옵니다.
-    fetch(`/get_food_ingredients?foodCode=${food['FOODID']}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(ingredient => {
-            var tr = document.createElement("tr");
-            tr.innerHTML = `<td>${ingredient['INGID']}</td>
-            <td>${ingredient['INGNAME_EN']}</td>
-            <td>${ingredient['1 person (g)']}</td>`;
-            ingredientTableBody.appendChild(tr);
-        });
-    });
-}
-
-function loadData() {
-    // loadFoodList();
-}
-
-function copyData() {
-    // Copy data functionality
 }
 
 // 엑셀로 내보내기
